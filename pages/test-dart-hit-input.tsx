c475805a853571a5910127b09808e16074a745c5
pmcg31 <seiffertrk@gmail.com>
2023-03-25 17:03:40 -0500

    Added text area to show dart hit on tester

--- pages/test-dart-hit-input.tsx
+++ pages/test-dart-hit-input.tsx
@@ -4,9 +4,34 @@ import {
   dartHitEventToPoints,
   dartHitEventToString
 } from '@/lib/dart-types';
+import { useRef } from 'react';
+
+let timeoutID: NodeJS.Timeout = undefined;
 
 export default function TestDartHitInput() {
+  const dartHitP = useRef();
+  const pointsP = useRef();
+
   function onDartHit(event: DartHitEvent) {
+    // If there is a timer out there,
+    // cancel it
+    if (timeoutID !== undefined) {
+      clearTimeout(timeoutID);
+    }
+
+    // Show the current values
+    (dartHitP.current as HTMLParagraphElement).textContent =
+      dartHitEventToString(event);
+    (
+      pointsP.current as HTMLParagraphElement
+    ).textContent = `${dartHitEventToPoints(event)}`;
+
+    // Clear current values in a second
+    timeoutID = setTimeout(() => {
+      (dartHitP.current as HTMLParagraphElement).textContent = '--';
+      (pointsP.current as HTMLParagraphElement).textContent = '--';
+    }, 1000);
+
     console.log(
       `onDartHit: ${dartHitEventToString(event)} (${dartHitEventToPoints(
         event
@@ -25,6 +50,19 @@ export default function TestDartHitInput() {
         }}
       >
         <DartHitInput autoResetToSingle={true} onDartHit={onDartHit} />
+        <div
+          style={{
+            display: 'flex',
+            gap: '20px',
+            padding: '10px',
+            color: 'white'
+          }}
+        >
+          <p>Dart Hit:</p>
+          <p ref={dartHitP}>--</p>
+          <p>Points:</p>
+          <p ref={pointsP}>--</p>
+        </div>
       </div>
     </main>
   );
