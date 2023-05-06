93fbd181704acecc64093d6b77c11173298a6c65
pmcg31 <seiffertrk@gmail.com>
2023-05-06 00:24:07 -0500

    Added dashboard link to header

--- components/ui/layout/layout-header.tsx
+++ components/ui/layout/layout-header.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Grid } from '@chakra-ui/react';
+import { AspectRatio, Flex } from '@chakra-ui/react';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
 import Logo from './dgwf_logo.svg';
@@ -9,18 +9,21 @@ export default function LayoutHeader({
   hideSignIn?: boolean;
 }): JSX.Element {
   return (
-    <Grid
+    <Flex
       backgroundColor={'var(--header-color)'}
       color={'white'}
       padding={'1rem'}
       alignItems={'center'}
-      templateColumns={'1fr auto'}
-      sx={{ '.cl-userButtonOuterIdentifier': { color: '#fff' } }}
+      justifyContent={'space-between'}
+      direction={'row'}
+      sx={{
+        '.cl-userButtonOuterIdentifier': { color: '#fff' }
+      }}
     >
       <Link href='/'>
         <AspectRatio
           ratio={1}
-          maxW={'3rem'}
+          width={'3rem'}
           opacity={'50%'}
           sx={{
             '.stroke_me': { stroke: '#fff' },
@@ -30,6 +33,13 @@ export default function LayoutHeader({
           <Logo />
         </AspectRatio>
       </Link>
+      <Flex flexGrow={1} p={'0 1rem'}>
+        <SignedIn>
+          <Link href='/dashboard'>
+            <p>Dashboard</p>
+          </Link>
+        </SignedIn>
+      </Flex>
       {!(hideSignIn != undefined && hideSignIn) && (
         <>
           <SignedIn>
@@ -45,6 +55,6 @@ export default function LayoutHeader({
           </SignedOut>
         </>
       )}
-    </Grid>
+    </Flex>
   );
 }
