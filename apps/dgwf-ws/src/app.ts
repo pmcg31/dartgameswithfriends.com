6fa70f9260dc8d94365be4583be3af25520c0897
pmcg31 <seiffertrk@gmail.com>
2023-05-27 12:46:54 -0500

    Added websocket server

---
+++ apps/dgwf-ws/src/app.ts
@@ -0,0 +1,49 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+import fs from 'fs';
+import https from 'https';
+import http from 'http';
+import express, { Request, Response } from 'express';
+import expressWs from 'express-ws';
+import { WebSocket } from 'ws';
+import { acceptConnection } from './conn-manager';
+
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY_FILE as fs.PathLike),
+  cert: fs.readFileSync(process.env.SSL_CERT_FILE as fs.PathLike)
+};
+const port = Number(process.env.LOCAL_PORT);
+const hostname = process.env.LOCAL_HOSTNAME;
+const sslEnable =
+  process.env.SSL_ENABLE && process.env.SSL_ENABLE.toLowerCase() === 'true';
+const exp = express();
+
+let server = null;
+if (sslEnable) {
+  server = https.createServer(options, exp);
+} else {
+  server = http.createServer(exp);
+}
+
+const { app /*, getWss, applyTo */ } = expressWs(exp, server);
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send("This is not the page you're looking for");
+});
+
+app.ws('/ws', async function (ws: WebSocket, req: Request) {
+  acceptConnection(
+    req.socket.remoteAddress as string,
+    req.socket.remotePort as number,
+    ws
+  );
+});
+
+if (sslEnable) {
+  server.listen(port, hostname);
+  console.log(`started server, url: https://${hostname}:${port}`);
+} else {
+  server.listen(port, hostname);
+  console.log(`started server, url: http://${hostname}:${port}`);
+}
---
+++ apps/dgwf-ws/src/conn-manager.ts
@@ -0,0 +1,139 @@
+import { CloseEvent, MessageEvent, WebSocket } from 'ws';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+type ConnMapValue = {
+  remoteAddress: string;
+  remotePort: number;
+  playerId?: string;
+};
+
+// type QueryMapValue = {
+//   ws: WebSocket;
+//   key: object;
+// };
+
+const connectionMap = new Map<WebSocket, ConnMapValue>();
+
+async function updatePlayerOnlineStatus(
+  playerId: string,
+  isOnline: boolean
+): Promise<void> {
+  try {
+    // Update the database
+    await prisma.player.update({
+      where: { id: playerId },
+      data: { isOnline, updatedAt: new Date() }
+    });
+
+    // Notify connected sockets
+    const msg = JSON.stringify({ mutation: 'updatePlayer' });
+    for (const ws of connectionMap.keys()) {
+      const tmp = connectionMap.get(ws);
+      if (tmp) {
+        console.log(`${tmp.remoteAddress}[${tmp.remotePort}] ===> ${msg}`);
+      }
+      ws.send(msg);
+    }
+  } catch (error) {
+    console.log('updatePlayerOnlineStatus: error:');
+    console.log(error);
+  }
+}
+
+function handleMessage(e: MessageEvent) {
+  const conn = connectionMap.get(e.target);
+  if (conn) {
+    const data = JSON.parse(e.data as string);
+    if (Object.prototype.hasOwnProperty.call(data, 'playerId')) {
+      if (conn.playerId) {
+        console.log(
+          `${conn.remoteAddress}[${conn.remotePort}] Player offline: ${conn.playerId}`
+        );
+        updatePlayerOnlineStatus(conn.playerId, false);
+      }
+      conn.playerId = data.playerId;
+      if (conn.playerId) {
+        console.log(
+          `${conn.remoteAddress}[${conn.remotePort}] Player online: ${data.playerId}`
+        );
+        updatePlayerOnlineStatus(conn.playerId, true);
+      }
+    } else if (Object.prototype.hasOwnProperty.call(data, 'usingQuery')) {
+      console.log(
+        `${conn.remoteAddress}[${conn.remotePort}] usingQuery: ${JSON.stringify(
+          data.usingQuery
+        )}`
+      );
+    } else if (Object.prototype.hasOwnProperty.call(data, 'releaseQuery')) {
+      console.log(
+        `${conn.remoteAddress}[${
+          conn.remotePort
+        }] releaseQuery: ${JSON.stringify(data.releaseQuery)}`
+      );
+    } else if (Object.prototype.hasOwnProperty.call(data, 'mutation')) {
+      console.log(
+        `${conn.remoteAddress}[${conn.remotePort}] mutation: ${
+          Object.keys(data.mutation)[0]
+        }`
+      );
+      // Forward to all connections
+      const msg = JSON.stringify({ mutation: Object.keys(data.mutation)[0] });
+      for (const ws of connectionMap.keys()) {
+        // Don't send back to originator
+        if (ws !== e.target) {
+          const tmp = connectionMap.get(ws);
+          if (tmp) {
+            console.log(`${tmp.remoteAddress}[${tmp.remotePort}] ===> ${msg}`);
+          }
+          ws.send(msg);
+        }
+      }
+    }
+  }
+}
+
+function handleClose(e: CloseEvent) {
+  const conn = connectionMap.get(e.target);
+  if (conn) {
+    if (conn.playerId) {
+      console.log(
+        `${conn.remoteAddress}[${conn.remotePort}] Player offline: ${conn.playerId}`
+      );
+      updatePlayerOnlineStatus(conn.playerId, false);
+    }
+
+    // Remove all query map values using this
+    // conection
+
+    // Remove from connection map
+    connectionMap.delete(e.target);
+
+    console.log(
+      `${conn.remoteAddress}[${conn.remotePort}] is outta here! total connections: ${connectionMap.size}`
+    );
+  }
+}
+
+export function acceptConnection(
+  remoteAddress: string,
+  remotePort: number,
+  ws: WebSocket
+) {
+  // Save the connection
+  connectionMap.set(ws, {
+    remoteAddress,
+    remotePort
+  });
+
+  // Handle messages from this connection
+  ws.addEventListener('message', handleMessage);
+
+  // Handle the closing of this connection
+  ws.addEventListener('close', handleClose);
+
+  console.log(
+    `Accepted connection from ${remoteAddress}[${remotePort}]; total connections: ${connectionMap.size}`
+  );
+}
